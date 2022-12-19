656cf8cc15898a4a5491d21e830a0aeb71bad3ee
User52525252 <90308361+User52525252@users.noreply.github.com>
2022-12-19 23:56:32 +0100

    Aditional Files
    Additional Files Required for school purposes

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Books (
+  Title varchar(255) NOT NULL,
+  Author varchar(255) NOT NULL,
+  ISBN varchar(255) NOT NULL PRIMARY KEY,
+  Publisher varchar(255) NOT NULL,
+  Status varchar(255) NOT NULL
+);
