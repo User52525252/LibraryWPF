99d8f749225061eb4daec8ec0f37e28ba1cf390e
User52525252 <90308361+User52525252@users.noreply.github.com>
2022-12-19 23:54:55 +0100

    Dodaj pliki projektów.

---
+++ LibraryWPF/LibraryManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryWPF
+{
+    public class LibraryManager
+    {   // String połączenia z bazą danych // należy zmienić przy użyciu innej bazy
+        private string _connectionString = "Data Source=DESKTOP-BA60I3I;Initial Catalog=Biblioteka?;Integrated Security=True";
+
+
+        /*public LibraryManager(string connectionString)
+        {
+             
+            _connectionString = connectionString;
+        }*/
+
+        public LibraryManager()
+        {
+        }
+
+        public void AddBook(Book book)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO Books (Title, Author, ISBN, Publisher, Status) VALUES (@Title, @Author, @ISBN, @Publisher, @Status)";
+                    command.Parameters.AddWithValue("@Title", book.Title);
+                    command.Parameters.AddWithValue("@Author", book.Author);
+                    command.Parameters.AddWithValue("@ISBN", book.ISBN);
+                    command.Parameters.AddWithValue("@Publisher", book.Publisher);
+                    command.Parameters.AddWithValue("@Status", book.Status);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void EditBook(Book book)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "UPDATE Books SET Title = @Title, Author = @Author, ISBN = @ISBN, Publisher = @Publisher, Status = @Status WHERE ISBN = @ISBN";
+                    command.Parameters.AddWithValue("@Title", book.Title);
+                    command.Parameters.AddWithValue("@Author", book.Author);
+                    command.Parameters.AddWithValue("@ISBN", book.ISBN);
+                    command.Parameters.AddWithValue("@Publisher", book.Publisher);
+                    command.Parameters.AddWithValue("@Status", book.Status);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteBook(string ISBN)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "DELETE FROM Books WHERE ISBN = @ISBN";
+                    command.Parameters.AddWithValue("@ISBN", ISBN);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<Book> SearchBooks(string searchText)
+        {
+            var books = new List<Book>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT * FROM Books WHERE Title LIKE @SearchText OR Author LIKE @SearchText OR ISBN LIKE @SearchText OR Publisher LIKE @SearchText";
+                    command.Parameters.AddWithValue("@SearchText", "%" + searchText + "%");
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var book = new Book
+                            {
+                                Title = reader["Title"].ToString(),
+                                Author = reader["Author"].ToString(),
+                                ISBN = reader["ISBN"].ToString(),
+                                Publisher = reader["Publisher"].ToString(),
+                                Status = reader["Status"].ToString()
+                            };
+                            books.Add(book);
+                        }
+                    }
+                }
+            }
+            return books;
+        }
+
+        public void Checkin(Book book)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "UPDATE Books SET Status = 'Dostepne' WHERE ISBN = @ISBN ";
+                    command.Parameters.AddWithValue("@Status", book.Status);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Checkout(Book book)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "UPDATE Books SET Status = 'NieDostepne' WHERE ISBN = @ISBN ";
+                    command.Parameters.AddWithValue("@Status", book.Status);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ LibraryWPF/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace LibraryWPF
+{
+
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public LibraryManager LibraryManager { get; private set; }
+
+        public void btnAddBook_Click(object sender, RoutedEventArgs e)
+        {// wywołuje funkcje służącą do dodawanie książek
+            LibraryManager = new LibraryManager();
+            if (!string.IsNullOrEmpty(TXT_title.Text) && !string.IsNullOrEmpty(TXT_author.Text) && !string.IsNullOrEmpty(TXT_isbn.Text) && !string.IsNullOrEmpty(TXT_publisher.Text) && !string.IsNullOrEmpty(TXT_status.Text)){ 
+                         
+                
+                var book = new Book
+                {
+                    Title = TXT_title.Text,
+                    Author = TXT_author.Text,
+                    ISBN = TXT_isbn.Text,
+                    Publisher = TXT_publisher.Text,
+                    Status = TXT_status.Text
+                };
+                LibraryManager.AddBook(book);
+            }
+
+
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        { // wywołuje funkcje służącą do wyszukiwania książek
+            LibraryManager = new LibraryManager();
+            dgBooks.ItemsSource = LibraryManager.SearchBooks(txtSearch.Text);
+        }
+
+        private void btnEditBook_Click(object sender, RoutedEventArgs e)
+        {// wywołuje funkcje służącą do edytowania książek
+            LibraryManager = new LibraryManager();
+            if (!string.IsNullOrEmpty(TXT_title.Text) && !string.IsNullOrEmpty(TXT_author.Text) && !string.IsNullOrEmpty(TXT_isbn.Text) && !string.IsNullOrEmpty(TXT_publisher.Text) && !string.IsNullOrEmpty(TXT_status.Text))
+            {
+                var book = new Book
+                {
+                    Title = TXT_title.Text,
+                    Author = TXT_author.Text,
+                    ISBN = TXT_isbn.Text,
+                    Publisher = TXT_publisher.Text,
+                    Status = TXT_status.Text
+                };
+                LibraryManager.EditBook(book);
+            }
+
+
+        }
+
+        private void btnDeleteBook_Click(object sender, RoutedEventArgs e)
+        {// wywołuje funkcje służącą do usuwania książek
+            LibraryManager = new LibraryManager();
+            LibraryManager.DeleteBook(TXT_isbn.Text);
+            
+        }
+
+        private void btnCheckOutBook_Click(object sender, RoutedEventArgs e)
+        {// wywołuje funkcje służącą do oznaczania książek jako wyporzyczone
+            LibraryManager = new LibraryManager();
+            if (!string.IsNullOrEmpty(TXT_title.Text) && !string.IsNullOrEmpty(TXT_author.Text) && !string.IsNullOrEmpty(TXT_isbn.Text) && !string.IsNullOrEmpty(TXT_publisher.Text) && !string.IsNullOrEmpty(TXT_status.Text))
+            { 
+                var book = new Book
+            {
+                Title = TXT_title.Text,
+                Author = TXT_author.Text,
+                ISBN = TXT_isbn.Text,
+                Publisher = TXT_publisher.Text,
+                Status = TXT_status.Text
+            };
+            LibraryManager.Checkout(book);
+            }
+
+        }
+
+        private void btnCheckInBook_Click(object sender, RoutedEventArgs e)
+        {// wywołuje funkcje służącą do oznaczania książęk jako dostępne
+            LibraryManager = new LibraryManager();
+            if (!string.IsNullOrEmpty(TXT_isbn.Text))
+            {
+
+                var book = new Book
+                {
+                    Title = TXT_title.Text,
+                    Author = TXT_author.Text,
+                    ISBN = TXT_isbn.Text,
+                    Publisher = TXT_publisher.Text,
+                    Status = TXT_status.Text
+                };
+                LibraryManager.Checkin(book);
+            }
+
+
+        }
+    }
+}
